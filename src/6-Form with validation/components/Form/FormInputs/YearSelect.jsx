5879cf9f45476cddd7d5b9ba52a532568d0b3079
Haniye Ghorbanian <haniyeghorbanian79@gmail.com>
2024-02-22 01:42:41 +0330

    year select valisation added

--- src/6-Form with validation/components/Form/FormInputs/YearSelect.jsx
+++ src/6-Form with validation/components/Form/FormInputs/YearSelect.jsx
@@ -1,18 +1,34 @@
 import { useState } from "react";
 
-const toPersianNumber = (number) => {
+const YearSelect = () => {
   const [year, setYear] = useState("");
+  const [isYearSelected, setIsYearSelected] = useState(false);
 
-  const persianNumbers = ["۰", "۱", "۲", "۳", "۴", "۵", "۶", "۷", "۸", "۹"];
-  return String(number).replace(/[0-9]/g, (match) => persianNumbers[+match]);
-};
 
-const YearSelect = ({ onChange }) => {
+
+
+  const toPersianNumber = (number) => {
+    const persianNumbers = ["۰", "۱", "۲", "۳", "۴", "۵", "۶", "۷", "۸", "۹"];
+    return String(number).replace(/[0-9]/g, (match) => persianNumbers[+match]);
+  };
   const years = [];
   for (let year = 1340; year <= 1389; year++) {
     years.push(year);
   }
 
+
+  
+
+
+
+  const handleYearChange = (e) => {
+    e.target.value !== "" ? setIsYearSelected(true) : setIsYearSelected(false);
+  };
+
+
+
+
+  console.log(isYearSelected);
   return (
     <div className="flex items-center ">
       <label htmlFor="birthYear" className="text-sm ml-3">
@@ -21,7 +37,7 @@ const YearSelect = ({ onChange }) => {
       <select
         id="birthYear"
         name="year"
-        onChange={onChange}
+        onChange={handleYearChange}
         className="border border-gray-300 rounded px-3 py-2 focus:outline-none border-slate-300 text-sm p-1"
       >
         {years.map((year, index) => (
