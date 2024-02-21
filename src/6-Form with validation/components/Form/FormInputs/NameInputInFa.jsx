c3a464fa5bc8eabc8afb73ce39524cd63b0abbe4
Haniye Ghorbanian <haniyeghorbanian79@gmail.com>
2024-02-22 00:39:55 +0330

    some fixes added to persain name input

--- src/6-Form with validation/components/Form/FormInputs/NameInputInFa.jsx
+++ src/6-Form with validation/components/Form/FormInputs/NameInputInFa.jsx
@@ -2,9 +2,8 @@ import { useState } from "react";
 
 const NameInputInFa = () => {
   const [persianName, setPersianName] = useState("");
-  const [warning, setWarning] = useState({ valid: null, message: "" });
-
-  const { valid, message } = warning;
+  const [isPersianNameValid, setIsPersianNameValid] = useState(null);
+  const [message, setMessage] = useState("");
 
   const handlePersianName = (e) => {
     const inputValue = e.target.value;
@@ -13,13 +12,12 @@ const NameInputInFa = () => {
     for (let i = 0; i < inputValue.length; i++) {
       const charCode = inputValue.charCodeAt(i);
       if (!(charCode >= 0x0600 && charCode <= 0x06ff)) {
-        setWarning({
-          valid: false,
-          message: "لطفا زبان کیبورد خود را روی فارسی قرار دهید",
-        });
+        setIsPersianNameValid(false);
+        setMessage("لطفا زبان کیبورد خود را روی فارسی قرار دهید");
       } else {
         setPersianName(inputValue);
-        setWarning({ valid: true, message: "" });
+        setIsPersianNameValid(true);
+        setMessage("");
       }
     }
   };
@@ -30,18 +28,20 @@ const NameInputInFa = () => {
         نام و نام‌خانوادگی به فارسی
       </label>
       <input
-        className={`w-full h-12 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:border-none rounded-3xl border-solid border-2 border-slate-200 rounded-2xl px-4 py-5 text-sm input ${
-          valid === false
-            ? "focus:ring-2 focus:ring-red-400 focus:border-none"
-            : valid === true
-            ? "focus:ring-2 focus:ring-green-400 focus:border-none"
-            : "focus:ring-2 focus:ring-blue-400 focus:border-none"
+        className={`w-full h-12 focus:transition focus:ease-in-out focus:duration-200  focus:outline-none focus:ring-2 focus:ring-blue-400 focus:border-none rounded-3xl border-solid border-2 border-slate-200 rounded-2xl px-4 py-5 text-sm input ${
+          isPersianNameValid === false
+            ? "focus:ring-2 focus:ring-red-400 focus:border-none border-red-400"
+            : isPersianNameValid === true
+            ? "focus:ring-2 focus:ring-green-400 focus:border-none border-green-400"
+            : "focus:ring-2 focus:ring-blue-400 focus:border-none border-slate-200"
         }`}
         type="text"
         id="nameFa"
         onChange={handlePersianName}
       />
-      {!valid && <span className="text-red-500 text-xs mr-5">{message}</span>}
+      {!isPersianNameValid && (
+        <span className="text-red-500 text-xs mr-5">{message}</span>
+      )}
     </div>
   );
 };
